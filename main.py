dd7efed82f139dfc9dd219b5e1c16500ef200d03
dawid <dawtom97@gmail.com>
2023-09-11 20:29:56 +0200

    v4

--- main.py
+++ main.py
@@ -17,4 +17,52 @@ def home_page():
        return "Nie działa"
 
 
-   
+@app.route("/country/<name>")
+def country_page(name):
+     
+      response = requests.get(f"https://restcountries.com/v3.1/name/{name}")
+      data = response.json()
+
+      currency_key = list(data[0]['currencies'].keys())[0]
+      native_name_key = list(data[0]['name']['nativeName'].keys())[0]
+      languages = list(data[0]['languages'].values())
+      
+      border_countries = ",".join(data[0]['borders'])
+      response_neighbours= requests.get(f"https://restcountries.com/v3.1/alpha?codes={border_countries}")
+      neighbours_data = list(response_neighbours.json())
+      neighbours = list(map(lambda neighbour: neighbour['name']['common'], neighbours_data))
+
+      print(neighbours)
+
+      country = {
+          "common_name":data[0]['name']['common'],
+          "native_name":data[0]['name']['nativeName'][native_name_key]['common'],
+          "currency_name": data[0]['currencies'][currency_key]['name'] if data[0]['currencies'] else "?",
+          "languages":", ".join(languages),
+          "population":data[0]['population'],
+          "region":data[0]['region'],
+          "capital":data[0]['capital'][0] if data[0]['capital'] else '?',
+          "sub_region":data[0]['subregion'],
+          'tld':data[0]['tld'],
+          "coat_of_arms":data[0]["coatOfArms"]['svg']
+      }
+
+     
+      return render_template("country.html",country=country, neighbours=neighbours)
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
